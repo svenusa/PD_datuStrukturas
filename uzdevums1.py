4937f63084163b1467afead6a6978a734354c4b8
svenusa <145644782+svenusa@users.noreply.github.com>
2025-02-25 13:30:58 +0200

    Update uzdevums1.py

---
+++ uzdevums1.py
@@ -0,0 +1,13 @@
+saraksts = ["Ķīna", "Amerika", "Latvija", "Lietuva", "Igaunija"]
+print(saraksts)
+
+saraksts.append("Brazīlija") #pievieno elementu
+print(saraksts)
+
+saraksts = ["Ķīna", "Amerika", "Latvija", "Lietuva", "Igaunija"]
+saraksts.pop(3) #izdzēš 3.elementu
+print(saraksts)
+
+saraksts = ["Ķīna", "Amerika", "Latvija", "Lietuva", "Igaunija"]
+saraksts.reverse() #apgriež saraksta secību
+print(saraksts)
